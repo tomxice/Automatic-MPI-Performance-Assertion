ba63ee3b81f5252681021605a8a099ecb6e82837
tomxice <tomxice@gmail.com>
2013-03-29 20:51:03 +0800

    1.parser can parse both loggpo and IMB data. 2.I think the Makefile SHOULD be this, but not verify yet.

---
+++ src/parser.h
@@ -0,0 +1,82 @@
+#ifndef PARSER_H
+#define PARSER_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define VERIFY 0
+
+#define MPS 1500
+typedef struct LoggpoPara {
+    int size;
+    double os;
+    double or;
+    double ov;
+    double sr;
+    double gap;
+    double rtt;
+    double rtt100;
+} LoggpoPara, *pLoggpoPara;
+
+#define PROC_N 10
+#define BYTE_N 30
+typedef struct IMB_BYTES_Para {
+    int bytes;
+    double t_min, t_max, t_avg;
+} Para;
+typedef struct IMBPara {
+    int n_barrier;
+    int n_bcast;
+    int n_reduce;
+    int n_gather;
+    int n_allreduce;
+    int n_allgather;
+    struct BarrierPara {
+        int proc; 
+        int n_byte;
+        double t_min, t_max, t_avg;
+    } barrier[PROC_N];
+    struct BcastPara {
+        int proc;
+        int n_byte;
+        Para para[BYTE_N];
+    } bcast[PROC_N];
+    struct ReducePara {
+        int proc;
+        int n_byte;
+        Para para[BYTE_N];
+    } reduce[PROC_N];
+    struct GatherPara {
+        int proc;
+        int n_byte;
+        Para para[BYTE_N];
+    } gather[PROC_N];
+    struct AllreducePara {
+        int proc;
+        int n_byte;
+        Para para[BYTE_N];
+    } allreduce[PROC_N];
+    struct AllgatherPara {
+        int proc;
+        int n_byte;
+        Para para[BYTE_N];
+    } allgather[PROC_N];
+} IMBPara, *pIMBPara;
+
+typedef enum IMBState {
+    init,
+    barrier,
+    bcast,
+    reduce,
+    gather,
+    allreduce,
+    allgather,
+    end,
+    data,
+    unknown
+} IMBState;
+
+extern LoggpoPara logps[MPS];
+extern IMBPara imb;
+
+#endif
